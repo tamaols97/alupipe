4eafd2b8e109245dd1fcfb90e64e01a005e917cc
nam <luongkhacnam222@gmail.com>
2024-04-12 02:53:34 +0700

    update

---
+++ client/app/components/commons/leaveMail2.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return <section>
+        Mail
+    </section>
+}
--- client/app/layout.js
+++ client/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
+
+import Footer from "./components/commons/footer";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
