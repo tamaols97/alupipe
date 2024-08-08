4627f14ea4ee8038dc084d95fff3042d34f16c09
HoangPzzz <137678673+HoangPzzz@users.noreply.github.com>
2024-08-08 20:38:15 +0700

    add home

---
+++ client/app/[lang]/home/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>asdasdasd</div>
+      <div>asdasdasd</div>
+      <div>asdasdasd</div>
+      <div>asdasdasd</div>
+      <div>asdasdasd</div>
+      <div>asdasdasd</div>
+      <div>asdasdasd</div>
+      <div>asdasdasd</div>
+    </div>
+  )
+}
+
+export default page
